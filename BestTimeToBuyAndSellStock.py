6178b0bbf3413dbddf442c6e073d3b53a0cc3c08
samj102 <s1jafri@ryerson.ca>
2024-08-02 03:32:19 -0400

    Added Solutions

---
+++ BestTimeToBuyAndSellStock.py
@@ -0,0 +1,16 @@
+#https://leetcode.com/problems/best-time-to-buy-and-sell-stock/ 
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        if not prices:
+                return 0
+
+        min_price = prices[0]
+        max_profit = 0
+
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
+
+        return max_profit
---
+++ ContainsDuplicte.py
@@ -0,0 +1,12 @@
+#https://leetcode.com/problems/contains-duplicate/
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        exists = set()
+        for i in nums: 
+            if i in exists:
+                return True
+            exists.add(i)
+        return False
+        
